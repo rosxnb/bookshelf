19c818898ab0d28e576ffd5b85cfd31d55619d99
Roshan Bharati <rosxnb@gmail.com>
2023-06-21 11:18:30 +0545

    Finish basic UI view
    Things to improve:
        - useage of scss variables
        - removal of hardcode values
        - font family needs redone
        - font sizes needs redone
        - page is not responsive

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,3 +1,4 @@
+// side navbar
 const sidebarHeight = document.querySelector('.sidebar').clientHeight;
 const collapsedItems = document.querySelectorAll('.navigation__item--collapse');
 collapsedItems.forEach(el => el.style.height = `${sidebarHeight}px`);
